71666eb3354d059abded7b5099f82be8c1be655f
Spycio.Kon <77606941+tiyeume25112004@users.noreply.github.com>
2022-10-26 18:29:38 +0700

    Update FPTU_CTF_SSTI.py

--- FPTU_CTF_SSTI.py
+++ FPTU_CTF_SSTI.py
@@ -1,6 +1,8 @@
 # --> dir(()), dir({}), dir([])
 # x = {}.get.__name__.__len__()
+# 3
 # {}.__lt__name__[x]
+# 'l'
 """
 konchan@SpycioKernel: dir(())
 ['__add__', '__class__', '__class_getitem__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewargs__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__rmul__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 'count', 'index']
@@ -11,3 +13,4 @@ konchan@SpycioKernel: dir([])
 >>> dir([])
 """
 print('tiep tuc')
+# Đã là SK thì phải biết SSTI UwU --> Payload all the thing to inject code
