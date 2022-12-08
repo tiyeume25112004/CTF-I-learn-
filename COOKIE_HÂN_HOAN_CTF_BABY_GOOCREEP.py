5f69321f619ef4dadc0ca208ffaa804b89b5523d
Spycio.Kon <77606941+tiyeume25112004@users.noreply.github.com>
2022-12-08 14:54:36 +0700

    Create COOKIE_HÂN_HOAN_CTF_BABY_GOOCREEP.py

---
+++ COOKIE_HÂN_HOAN_CTF_BABY_GOOCREEP.py
@@ -0,0 +1,13 @@
+#bạn có thể sài tool https://enkhee-osiris.github.io/Decoder-JSFuck/ để decode jsfuck :V
+#có js thì dùng https://beautifier.io để xem cho dễ 
+print(ord('@'))
+print(chr(64))
+#Tui không biết code tool nhưng code từng dòng thì ok UwU
+# làm toán tính 100 và 109
+print(chr(100))
+print(chr(109))
+print(chr(78))
+print(chr(105))
+a='dr0Wss@p3rucreSr3pus'[::-1]
+print(a)
+#dùng list+for cũng được cho khỏi rối nhưng t thích đc ko
