8f30aabb63892ec868244c87869c03d893445580
Spycio.Kon <77606941+tiyeume25112004@users.noreply.github.com>
2023-02-05 02:19:16 +0700

    Create BBCTF_CTF_MEANING_OF_LIFE.py

---
+++ BBCTF_CTF_MEANING_OF_LIFE.py
@@ -0,0 +1,24 @@
+import requests
+import base64
+url="http://misc.bbctf.fluxus.co.in:2002"
+a='9'
+for i in range(1,50):
+    # u=a*i
+    # print(u)
+    data={"key_num":i}
+    r=requests.post(url,data=data)
+    b=r.text.split("</p>")[0].split("</b> ")
+    print(base64.b64decode(b[1]))
+    print(i)
+    
+# 999999999999999999999
+# b'https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=195s'
+# 21
+# 9999999999999999999999
+# b'https://www.youtube.com/watch?v=FIUbRJkKjlE' -> this is link, download video and decode morse
+# 22
+# 99999999999999999999999
+# b'https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=3s'
+# 23
+# 999999999999999999999999
+# b'https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=39s
