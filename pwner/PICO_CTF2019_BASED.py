6b37ba5bd17c0200627d59aa468d297c48be3b1c
Spycio.Kon <77606941+tiyeume25112004@users.noreply.github.com>
2023-03-07 04:22:22 +0700

    Update PICO_CTF2019_BASED.py

--- pwner/PICO_CTF2019_BASED.py
+++ pwner/PICO_CTF2019_BASED.py
@@ -54,3 +54,22 @@ while s is not None:
     s = get_base_encoded_str(r)
 
 print r.recvall()
+"""
+from pwn import *
+
+sh = remote('2019shell1.picoctf.com', 20836)
+
+binary_data = sh.recvuntil('Input:\n').split('\n')[2].split(' ')[3:-3]
+binary_data = ''.join(map(lambda x: chr(int(x, 2)), binary_data))
+sh.sendline(binary_data)
+
+oct_data = sh.recvuntil('Input:\n').split('\n')[0].split('the  ')[-1].split(' as')[0].split(' ')
+oct_data = ''.join(map(lambda x: chr(int(x, 8)), oct_data))
+sh.sendline(oct_data)
+
+hex_data = sh.recvuntil('Input:\n').split('\n')[0].split('the ')[-1].split(' as')[0]
+hex_data = hex_data.decode('hex')
+sh.sendline(hex_data)
+
+sh.interactive()
+"""
