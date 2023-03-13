a90bdb35ffcfbbb00a5d9876a2588716ec576a47
Spycio.Kon <77606941+tiyeume25112004@users.noreply.github.com>
2023-03-13 08:23:18 +0700

    Create  UTCTF_CACULATOR.py

---
+++ UTCTF_CACULATOR.py
@@ -0,0 +1,14 @@
+import random, os
+password = open("password.txt").read()
+os.remove("password.txt") # No more reading the file!
+
+solution = random.getrandbits(32)
+
+answer = input()
+result = eval(answer, {})
+
+if result == solution:
+    print(f"{result}, correct!  The password is '{password}'.")
+else:
+    print(f"Result: {result}.  The correct answer was {solution}.")
+#solution __import__('__main__').password
